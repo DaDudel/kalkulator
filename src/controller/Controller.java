f83c2318eb8b991e6b07a6cc32fdc48946f26a38
Maciej Duda <mdud1999@gmail.com>
2021-04-05 20:30:57 +0200

    Javadoc comments

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,12 +6,20 @@ import model.Operation;
 
 import javafx.scene.control.TextField;
 
+/**
+ * The type Controller.
+ */
 public class Controller {
     private Operation op = new Operation();
 
     @FXML
     private TextField numbers;
 
+    /**
+     * Equals.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void equals(ActionEvent actionEvent) {
         if(op.getC()!=null) {
@@ -20,6 +28,12 @@ public class Controller {
             numbers.setText(op.getC().toString());
         }
     }
+
+    /**
+     * Add.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void add(ActionEvent actionEvent) {
         op.setA(Double.parseDouble(numbers.getText()));
@@ -28,6 +42,11 @@ public class Controller {
         numbers.setText("");
     }
 
+    /**
+     * Minus.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void minus(ActionEvent actionEvent) {
         op.setA(Double.parseDouble(numbers.getText()));
@@ -36,6 +55,11 @@ public class Controller {
         numbers.setText("");
     }
 
+    /**
+     * Multiply.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void multiply(ActionEvent actionEvent) {
         op.setA(Double.parseDouble(numbers.getText()));
@@ -44,6 +68,11 @@ public class Controller {
         numbers.setText("");
     }
 
+    /**
+     * Divide.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void divide(ActionEvent actionEvent) {
         op.setA(Double.parseDouble(numbers.getText()));
@@ -52,6 +81,11 @@ public class Controller {
         numbers.setText("");
     }
 
+    /**
+     * Sets minus.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void setMinus(ActionEvent actionEvent) {
         if(numbers.getText().startsWith("-")) {
@@ -61,56 +95,111 @@ public class Controller {
         }
     }
 
+    /**
+     * Sets 0.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set0(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"0");
     }
 
+    /**
+     * Sets 1.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set1(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"1");
     }
 
+    /**
+     * Sets 2.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set2(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"2");
     }
 
+    /**
+     * Sets 3.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set3(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"3");
     }
 
+    /**
+     * Sets 4.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set4(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"4");
     }
 
+    /**
+     * Sets 5.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set5(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"5");
     }
 
+    /**
+     * Sets 6.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set6(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"6");
     }
 
+    /**
+     * Sets 7.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set7(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"7");
     }
 
+    /**
+     * Sets 8.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set8(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"8");
     }
 
+    /**
+     * Sets 9.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void set9(ActionEvent actionEvent) {
         numbers.setText(numbers.getText()+"9");
     }
 
+    /**
+     * Sets point.
+     *
+     * @param actionEvent the action event
+     */
     @FXML
     public void setPoint(ActionEvent actionEvent) {
         if(numbers.getText().indexOf('.')==-1) {
--- src/model/Operation.java
+++ src/model/Operation.java
@@ -1,46 +1,95 @@
 package model;
 
+/**
+ * The type Operation.
+ */
 public class Operation {
     private Double a;
     private Double b;
     private Double c;
     private String symbol;
 
+    /**
+     * Gets a.
+     *
+     * @return the a
+     */
     public Double getA() {
         return a;
     }
 
+    /**
+     * Sets a.
+     *
+     * @param a the a
+     */
     public void setA(Double a) {
         this.a = a;
     }
 
+    /**
+     * Gets b.
+     *
+     * @return the b
+     */
     public Double getB() {
         return b;
     }
 
+    /**
+     * Sets b.
+     *
+     * @param b the b
+     */
     public void setB(Double b) {
         this.b = b;
     }
 
+    /**
+     * Gets c.
+     *
+     * @return the c
+     */
     public Double getC() {
         return c;
     }
 
+    /**
+     * Sets c.
+     *
+     * @param c the c
+     */
     public void setC(Double c) {
         this.c = c;
     }
 
+    /**
+     * Gets symbol.
+     *
+     * @return the symbol
+     */
     public String getSymbol() {
         return symbol;
     }
 
+    /**
+     * Sets symbol.
+     *
+     * @param symbol the symbol
+     */
     public void setSymbol(String symbol) {
         this.symbol = symbol;
     }
 
+    /**
+     * Instantiates a new Operation.
+     */
     public Operation() {
     }
 
+    /**
+     * Calculate.
+     */
     public void calculate(){
         switch (symbol){
             case "+":
